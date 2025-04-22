84196f2bf0ac7eee769ab5b62e95782a3324a975
Marcin Rompczyk <marcinr@stud.ntnu.no>
2025-04-22 13:45:25 +0200

    On branch main  Changes to be committed: 	new file:   .gitmodules 	new file:   qube_driver 	new file:   src/pid_controller_msgs/CMakeLists.txt 	new file:   src/pid_controller_msgs/package.xml 	new file:   src/pid_controller_msgs/srv/SetReference.srv 	new file:   src/qube_bringup/launch/bringup.launch.py 	new file:   src/qube_bringup/package.xml 	new file:   src/qube_bringup/qube_bringup/__init__.py 	new file:   src/qube_bringup/resource/qube_bringup 	new file:   src/qube_bringup/setup.cfg 	new file:   src/qube_bringup/setup.py 	new file:   src/qube_bringup/test/test_copyright.py 	new file:   src/qube_bringup/test/test_flake8.py 	new file:   src/qube_bringup/test/test_pep257.py 	new file:   src/qube_bringup/urdf/controlled_qube.urdf.xacro 	new file:   src/qube_controller/package.xml 	new file:   src/qube_controller/qube_controller/__init__.py 	new file:   src/qube_controller/qube_controller/__pycache__/__init__.cpython-312.pyc 	new file:   src/qube_controller/qube_controller/__pycache__/pid_node.cpython-312.pyc 	new file:   src/qube_controller/qube_controller/pid_node.py 	new file:   src/qube_controller/qube_controller/reference_input_node.py 	new file:   src/qube_controller/resource/qube_controller 	new file:   src/qube_controller/setup.cfg 	new file:   src/qube_controller/setup.py 	new file:   src/qube_controller/test/test_copyright.py 	new file:   src/qube_controller/test/test_flake8.py 	new file:   src/qube_controller/test/test_pep257.py 	new file:   src/qube_description/LICENSE 	new file:   src/qube_description/launch/view_qube.launch.py 	new file:   src/qube_description/package.xml 	new file:   src/qube_description/qube_description/__init__.py 	new file:   src/qube_description/resource/qube_description 	new file:   src/qube_description/setup.cfg 	new file:   src/qube_description/setup.py 	new file:   src/qube_description/test/test_copyright.py 	new file:   src/qube_description/test/test_flake8.py 	new file:   src/qube_description/test/test_pep257.py 	new file:   src/qube_description/urdf/qube.macro.xacro 	new file:   src/qube_description/urdf/qube.urdf.xacro

---
+++ src/qube_bringup/launch/bringup.launch.py
@@ -0,0 +1,69 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, ExecuteProcess
+from launch.substitutions import LaunchConfiguration, Command
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def launch_setup(*args):
+    # 🔧 Definere PID-kontroller kommando
+    pid_cmd_str = (
+        f'source ~/Qube_ws/Mini-prosjekt/install/setup.bash && '  #     Hvis nødevendig, endre path til setup.bash på denne linjen. Default er ~/Mini-prosjekt/install/setup.bash
+        f'ros2 run qube_controller pid_node '
+    )
+    pid_cmd = ['gnome-terminal', '--', 'bash', '-c', pid_cmd_str]
+    
+    # 🔧Definere Input-node
+    input_cmd_str = (
+        'source ~/Qube_ws/Mini-prosjekt/install/setup.bash &&' #     Hvis nødevendig, endre path til setup.bash på denne linjen. Default er ~/Mini-prosjekt/install/setup.bash
+        'ros2 run qube_controller reference_input_node; exec bash'
+    )
+    input_cmd = ['gnome-terminal', '--', 'bash', '-c', input_cmd_str]
+    
+    return [
+        ExecuteProcess(cmd=pid_cmd, shell=False),
+        ExecuteProcess(cmd=input_cmd, shell=False),
+    ]
+    
+
+def generate_launch_description():
+    
+    # Paths
+    pkg_bringup = get_package_share_directory('qube_bringup')
+    urdf_path = os.path.join(pkg_bringup, 'urdf', 'controlled_qube.urdf.xacro')
+    pkg_driver_path= os.path.join(
+            get_package_share_directory('qube_driver'), 
+            'launch', 
+            'qube_driver.launch.py'
+    )
+
+    robot_description = Command(['xacro ', urdf_path])
+   
+    return LaunchDescription([
+        
+        #       Starte to separate terminaler for PID_node og reference_input
+        OpaqueFunction(function=launch_setup),
+        
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(pkg_driver_path)
+        ),
+       
+       #        Starte robot_sate_publisher
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': robot_description}]
+        ),
+        
+        #       Starte rciv2 visualisering
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            arguments=[],
+            output='screen',
+            condition=None
+        ),
+        
+    ])
